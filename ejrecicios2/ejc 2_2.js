64331f4f0c1cbf24ffd3992757437e30de4a0569
Antoniovelascodev <antoniovelasco.dev@gmail.com>
2025-10-27 12:47:07 +0100

    cambios y ejc

---
+++ ejrecicios2/ejc 2_2.js
@@ -0,0 +1,6 @@
+let contador= 0; 
+const numero = 10 ; 
+
+for(contador = 1; numero >= contador ; contador++) {
+    console.log(`El cuadrado de ${contador} es ${contador*contador} `); 
+}
---
+++ ejrecicios2/ejc2_1.js
@@ -0,0 +1,23 @@
+const n =10; 
+let contador = 0; 
+
+while(contador < n) {
+    console.log(`Iteración ${contador+1}`); 
+
+    contador = contador +1 ; 
+}         
+
+for (let contador2 = 0; contador2< n ; contador2++){
+
+    console.log (`Iteración ${contador2}`)
+
+}
+
+letcontador3=0; 
+
+do{
+    console.log(`Iteración ${contador3 + 1}`); 
+    contador3++; 
+
+    
+} while ( contador3<n)
