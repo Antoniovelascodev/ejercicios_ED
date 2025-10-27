1dc8f49fbc8411d0001fa14369397ee771af7a6c
Antonio Velasco <antoniovelasco.dev@gmail.com>
2025-10-27 12:49:42 +0100

    Update ejc 2_2.js

--- ejercicios2/ejc 2_2.js
+++ ejercicios2/ejc 2_2.js
@@ -1,6 +1,8 @@
 let contador= 0; 
 const numero = 10 ; 
 
+//esto es para saber el cuadrado de 10 numeros empezando desde contador = 0 
+
 for(contador = 1; numero >= contador ; contador++) {
     console.log(`El cuadrado de ${contador} es ${contador*contador} `); 
 }
