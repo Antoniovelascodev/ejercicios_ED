a2ec7dc83042e061186d03a06dde9d89230fcfc5
Antoniovelascodev <antoniovelasco.dev@gmail.com>
2025-11-10 12:43:55 +0100

    updt

--- pirámide.js
+++ pirámide.js
@@ -1,8 +1,13 @@
-//  *****
-//  .***
-//  ..* 
-// . = espacio
+const prompt = require('prompt-sync')();
 
-for( ){
-    2*(altura-1) 
+let altura = prompt("Dime la altura de la pirámide: "); 
+let contador = 0; 
+
+while ( contador < altura ){
+    
+    process.stdout.write ( '-' .repeat (altura-1-contador)); //Escribir sin salto de linea  
+    process.stdout.write ( '*' .repeat (2*contador+1)); //Escribir sin salto de linea  
+    process.stdout.write ('\n'); //saltos de linea
+    contador++; 
+
 }
