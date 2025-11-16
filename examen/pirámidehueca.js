55aa3fa72e1f4afa108f1514f4e374d4e1005fb1
Antoniovelassco <antoniovelasco.dev@gmail.com>
2025-11-16 16:51:26 +0100

    update ejc

---
+++ examen/pirámidehueca.js
@@ -0,0 +1,32 @@
+const prompt = require('prompt-sync')();
+
+let altura = prompt("Dime la altura de la pirámide: "); 
+
+
+if (altura>0 ){// inicio de pirámide
+        console.log(" ".repeat(altura)+" *");
+    }
+
+for ( let i = 0; i<altura-2; i++){// vacio de pirámide
+   
+    if ( i < altura ){ 
+        process.stdout.write(" ".repeat(altura-i) + "*");
+    
+        if (i>=0){
+            let j = -1; 
+            process.stdout.write(" ".repeat((i+i)-j) + "*"); 
+        }
+
+        console.log();
+
+    }
+}
+
+
+if(altura>0){
+    let k = -1;
+    console.log(" " + " *".repeat(altura));
+}
+
+
+
